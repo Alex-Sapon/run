a34246ac807de2761a3ccde8746f33199075e5cf
Aleksandr_Saponchik <alexsapon@gmail.com>
2021-10-20 19:56:27 +0300

    init project

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,17 +1,19 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu__item'),
-    hamburger = document.querySelector('.hamburger');
+// JS-функция определения поддержки WebP
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    }
+    
+    testWebP(function (support) {
+    
+    if (support == true) {
+    document.querySelector('body').classList.add('webp');
+    }else{
+    document.querySelector('body').classList.add('no-webp');
+    }
     });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
