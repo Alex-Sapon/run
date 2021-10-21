2634bb7d2fd0c1866ff5514243003a1ba8dd7832
Aleksandr_Saponchik <alexsapon@gmail.com>
2021-10-21 21:56:44 +0300

    add style and html for the slider

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,19 +1,25 @@
 // JS-функция определения поддержки WebP
-
 function testWebP(callback) {
-
     var webP = new Image();
     webP.onload = webP.onerror = function () {
-    callback(webP.height == 2);
+        callback(webP.height == 2);
     };
     webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-    }
-    
-    testWebP(function (support) {
-    
+}
+testWebP(function (support) {
     if (support == true) {
-    document.querySelector('body').classList.add('webp');
-    }else{
-    document.querySelector('body').classList.add('no-webp');
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
     }
+});
+
+// Click slider
+$(document).ready(function(){
+    $('.slider__body').slick({
+        infinity: true,
+        dots: false,
+        slidesToShow: 1
     });
+});
+
