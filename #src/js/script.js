59a5015a68ab96a021d215e01805ff67c2e5e4bf
Aleksandr_Saponchik <alexsapon@gmail.com>
2021-10-24 16:09:24 +0300

    add adaptive in the sections: header, promo, advantage, consultation, slider, catalog

--- #src/js/script.js
+++ #src/js/script.js
@@ -14,12 +14,43 @@ testWebP(function (support) {
     }
 });
 
-// Click slider
+
 $(document).ready(function(){
+    // Click slider
     $('.slider__body').slick({
         infinity: true,
         dots: false,
-        slidesToShow: 1
+        slidesToShow: 1,
+        responsive: [
+            {
+              breakpoint: 991.98,
+              settings: {
+                slidesToShow: 1,
+                dots: true,
+                arrows: false
+              }
+            }
+          ]
     });
+    
+    // Tabs
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div._container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__desc').eq(i).toggleClass('catalog-item__desc_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
 
