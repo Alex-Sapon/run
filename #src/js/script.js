27374922ecd5a84524a4ee5b2f6da0c5c889a14d
Aleksandr_Saponchik <alexsapon@gmail.com>
2021-10-27 20:16:43 +0300

    add features in the inputs

--- #src/js/script.js
+++ #src/js/script.js
@@ -68,5 +68,35 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn();
         })
     })
+
+    // Validate
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                  required: true,
+                  email: true
+                }
+              },
+              messages: {
+                name: "Пожалуйста, введите свое имя",
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+              }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#order .form');
+    validateForms('#consultation .form');
+
+    // Mask unput
+    $('input[name=phone]').mask('+999(99) 999-99-99');
+    
 });
 
